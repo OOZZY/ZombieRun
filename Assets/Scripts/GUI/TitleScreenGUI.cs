c4c229225263796efa3fa4adc039ed2efd68e34d
oozzy <oozzy.tlo@gmail.com>
2016-03-06 20:38:19 -0800

    Implemented title screen and introduction scene

---
+++ Assets/Scripts/GUI/TitleScreenGUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("IntroductionScene");
+		}
+	}
+}
