6afb03f4f992a4e7dd428c835c0a11a732b2f2c3
leeraymond <rkl9@sfu.ca>
2016-03-14 13:19:55 -0700

    Add sprites, house, lab, power plant, cell tower, enter house feature

---
+++ Assets/Scripts/Level/Level01EnterMap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level01EnterMap : MonoBehaviour {
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerStay2D(Collider2D collider) {
+		if ((Input.GetKeyDown (KeyCode.DownArrow))) {
+			if (collider.gameObject == player) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene ("Level01");
+			}
+		}
+	}
+}
