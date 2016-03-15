b7db3da12b0deed19a75b64b578937392a1e7bd8
leeraymond <rkl9@sfu.ca>
2016-03-14 22:26:58 -0700

    Added triggers for house, powerplant, cell tower, lab, computer. Change scenes and cut scenes for using internet. Included password list item sprite.

---
+++ Assets/Scripts/GUI/PhoneScene.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhoneScene : MonoBehaviour {
+	List<GameObject> slides = new List<GameObject>();
+	int currentSlideNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; ; ++i) {
+			GameObject slide = GameObject.Find ("PhoneScene" + i);
+			if (slide == null) {
+				break;
+			} else {
+				slides.Add (slide);
+			}
+		}
+
+		slides [0].GetComponent<SpriteRenderer> ().enabled = true;
+		for (int i = 1; i < slides.Count; ++i) {
+			slides [i].GetComponent<SpriteRenderer> ().enabled = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			if (currentSlideNumber >= slides.Count - 1) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene ("Level01");
+				return;
+			}
+			slides [currentSlideNumber].GetComponent<SpriteRenderer> ().enabled = false;
+			currentSlideNumber++;
+			slides [currentSlideNumber].GetComponent<SpriteRenderer> ().enabled = true;
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && currentSlideNumber > 0) {
+			slides [currentSlideNumber].GetComponent<SpriteRenderer> ().enabled = false;
+			currentSlideNumber--;
+			slides [currentSlideNumber].GetComponent<SpriteRenderer> ().enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Level/UseComputer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseComputer : MonoBehaviour {
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerStay2D(Collider2D collider) {
+		if ((Input.GetKeyDown (KeyCode.E))) {
+			if (collider.gameObject == player) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene ("UseComputer");
+			}
+		}
+	}
+}
