e4fd9aea851f67bc379e120e5e28a0d39d15be90
oozzy <oozzy.tlo@gmail.com>
2016-03-20 19:07:12 -0700

    Implement items

---
+++ Assets/Scripts/Items/ItemTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemTrigger : MonoBehaviour {
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject == player) {
+			if (name.StartsWith("health")) {
+				player.GetComponent<PlayerBehavior> ().health += 10;
+			}
+			if (name.StartsWith("ammo")) {
+				player.GetComponent<PlayerBehavior> ().ammo += 10;
+			}
+			Destroy (gameObject);
+		}
+	}
+}
