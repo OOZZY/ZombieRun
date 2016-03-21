63b3f98837bef38253b4c9c39af0fa1d28c08e83
oozzy <oozzy.tlo@gmail.com>
2016-03-21 11:42:31 -0700

    Add more state to global state: hasKey

--- Assets/Scripts/Global/GlobalState.cs
+++ Assets/Scripts/Global/GlobalState.cs
@@ -8,6 +8,7 @@ public class GlobalState : MonoBehaviour {
 	public Vector3 level01SpawnPosition = new Vector3(-8.23f, 0, 0);
 	public bool hasCureInfo = false;
 	public Vector3 labSpawnPosition = new Vector3 (-0.8180754f, -2.75621f, 0);
+	public bool hasKey = false;
 
 	// Use this for initialization
 	void Start () {
